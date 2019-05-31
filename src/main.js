cf96f4851b98643e33d4c20b95abf94d3ee04a57
Ryan Plas <ryan.plas@defpoint.com>
2019-05-31 00:49:03 -0400

    Fix Print Stylesheet

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const options = {
   specs: ["fullscreen=yes", "titlebar=yes", "scrollbars=yes"],
   styles: [
     "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css",
-    "/print.css"
+    process.env.BASE_URL + "print.css"
   ]
 };
 
